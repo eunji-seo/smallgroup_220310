d5955d450a3e7f99d089709045f6d7b6f30d6260
eunji-seo <toma1019@gmail.com>
2022-04-08 22:08:38 -0700

    커

--- src/main/java/com/smallgroup/common/SHA256.java
+++ src/main/java/com/smallgroup/common/SHA256.java
@@ -3,9 +3,14 @@ package com.smallgroup.common;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.springframework.stereotype.Component;
+@Component
 public class SHA256 {
 
+	private final static String SHA256_SALT= "smallgroup"; 
+	
 	 public String encrypt(String text)  {
+		 text += SHA256_SALT;
 	        MessageDigest md = null;
 			try {
 				md = MessageDigest.getInstance("SHA-256");
@@ -24,6 +29,12 @@ public class SHA256 {
 	            builder.append(String.format("%02x", b));
 	        }
 	        return builder.toString();
+	    }
+	    
+	    public static void main(String[]args) {
+	    	SHA256 s = new SHA256();
+	    	String sha = s.encrypt("1");
+	    	
 	    }
 
 }
